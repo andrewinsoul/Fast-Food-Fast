9030e2d7921f94cc28f2b13e86f408a921c1cff3
andrewinsoul <andrewinsoul@gmail.com>
2018-09-10 01:13:16 -0700

    add some images and include responsiveness

--- UI/assets/scripts/main.js
+++ UI/assets/scripts/main.js
@@ -1,12 +1,9 @@
-const navToggler = document.getElementById('toggler');
-const navBtn = document.getElementById('btn-i');
-const assitLink = document.getElementById('support-links');
-function openToggler() {
-  navBtn.className = 'fa fa-close';
-  assitLink.style.display = 'block';
-  navToggler.onclick = "closeToggler()"
+function toggler() {
+  const navElement = document.getElementById('Topnav');
+  if (navElement.className === 'topnav') {
+    navElement.className += ' responsive';
+  }
+  else {
+    navElement.className = 'topnav';
+  }
 }
- function closeToggler() {
-   navBtn.className = 'fa fa-bars'
-   assitLink.style.display = 'none'
- }
