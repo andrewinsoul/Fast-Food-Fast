78bc2f6a3b8d7501b69cb4b7b363899b673831aa
andrewinsoul <andrewinsoul@gmail.com>
2018-09-17 13:13:28 -0700

    validate user input [Finishes #160566613]

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
         "linebreak-style": 0,
         "class-methods-use-this": 0,
         "no-console": 0,
+        "no-restricted-globals": 0,
     }
 };
--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import orderController from '../controllers/orders';
+import helperFunction from '../middlewares/helperFunction';
+import validator from '../middlewares/validateFunction';
 
 const orderRouter = express.Router();
 
 orderRouter
-  .post('/orders', orderController.placeAnOrder)
-  .get('/orders/:id', orderController.getAnOrder)
+  .post('/orders', validator.placeOrder, orderController.placeAnOrder)
+  .get('/orders/:id', validator.getOrder, helperFunction, orderController.getAnOrder)
   .get('/orders', orderController.getAllOrders)
-  .put('/orders/:id', orderController.updateAnOrder);
+  .put('/orders/:id', validator.updateOrder, helperFunction, orderController.updateAnOrder);
 export default orderRouter;
