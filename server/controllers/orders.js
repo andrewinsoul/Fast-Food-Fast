1320c476cc655c08f897d294d771a7f1dfbe03a0
Okoye Andrew <andrewinsoul@gmail.com>
2018-09-19 10:49:48 -0700

    Revert "setup travis and add badges to README"
    This reverts commit e5885dc81fabec1fb91ee6a58f28d8419c7beeb9.

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,25 +1,10 @@
 import orderData from '../models/order';
 
-/**
- * @description contains methods that manipulates data from order model
- */
 class Order {
-  /**
-   * @description - method that fetches all orders
-   * @param {object} req - The request object
-   * @param {object} res - The response object
-   * @returns {object} - status code and server message
-   */
   getAllOrders(req, res) {
     return res.status(200).send({ status: 'success', message: orderData });
   }
 
-  /**
-   * @description - method that adds an order to array of orders
-   * @param {object} req - The request object
-   * @param {object} res - The response object
-   * @returns {object} -status code and server message
-   */
   placeAnOrder(req, res) {
     const {
       name, order, address, status,
@@ -35,34 +20,16 @@ class Order {
     return res.status(201).send({ status: 'success', order: orderDetails });
   }
 
-  /**
-   * @description - method that fetches an order based on id param
-   * @param {object} req - The request object
-   * @param {object} res - The response object
-   * @returns {object} - status code and server message
-   */
   getAnOrder(req, res) {
-    const index = orderData.findIndex(
-      order => order.id === Number(req.params.id)
-    );
+    const index = orderData.findIndex(order => order.id === Number(req.params.id));
     return res.status(200).send({ status: 'success', order: orderData[index] });
   }
 
-  /**
-   * @description - method that updates the status of an order based on id param
-   * @param {object} req - The request object
-   * @param {object} res - The response object
-   * @returns {object} - status code and server message
-   */
   updateAnOrder(req, res) {
     const message = 'status of order successfully updated';
-    const index = orderData.findIndex(
-      order => order.id === Number(req.params.id)
-    );
+    const index = orderData.findIndex(order => order.id === Number(req.params.id));
     orderData[index].status = req.body.status;
-    return res.status(200).send({
-      status: 'success', message, order: orderData
-    });
+    return res.status(200).send({ status: 'success', message });
   }
 }
 export default new Order();
