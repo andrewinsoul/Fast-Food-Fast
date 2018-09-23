3509a35cf4141574902cab01e669bd9ca576687d
andrewinsoul <andrewinsoul@gmail.com>
2018-09-23 01:24:51 -0700

    modify gitignore and remove server folder

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "linebreak-style": 0,
-        "class-methods-use-this": 0,
-        "no-console": 0,
-        "no-restricted-globals": 0,
-    }
-};
