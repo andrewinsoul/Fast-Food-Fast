20f90e8ab7d30ab9257b2ea81f545f83b8efe619
andrewinsoul <andrewinsoul@gmail.com>
2018-10-08 10:57:40 -0700

    add description column in menu table

--- dbServer/sql/models/menu.js
+++ dbServer/sql/models/menu.js
@@ -3,6 +3,7 @@ const menu = `
     foodId serial primary key, 
     food varchar(350),
     price integer,
+    description varchar(450),
     userId integer,
     foreign key(userId) references users(userId) 
     on delete cascade on update cascade
--- dbServer/test/b-menu-test.js
+++ dbServer/test/b-menu-test.js
@@ -31,10 +31,10 @@ describe('Fast-Food-Fast backend tests with postgres database for menu model', (
         .set('x-access-token', adminToken)
         .send({
           foodName: 'Indomie and Coke',
+          description: 'Indomie coated with two pieces of egg omelette and a coca-cola softdrink',
           price: 1200
         })
         .end((err, res) => {
-          console.log(adminToken);
           expect(res).to.have.status(201);
           expect(res.body.status).to.eql('success');
           done();
@@ -60,7 +60,8 @@ describe('Fast-Food-Fast backend tests with postgres database for menu model', (
         .post('/api/v1/menu')
         .set('x-access-token', adminToken)
         .send({
-          foodName: 'food name'
+          foodName: 'food name',
+          description: 'description'
         })
         .end((err, res) => {
           expect(res).to.have.status(400);
@@ -75,11 +76,59 @@ describe('Fast-Food-Fast backend tests with postgres database for menu model', (
         .set('x-access-token', adminToken)
         .send({
           foodName: 'food-name',
-          price: 1200
+          price: 1200,
+          description: 'description'
         })
         .end((err, res) => {
           expect(res).to.have.status(400);
           expect(res.body.error).to.eql('Numbers and special characters not allowed as part of food name, only letters and spaces are allowed');
+          done();
+        });
+    });
+
+    it('should return code 400 with error message when user tries to create menu without description field', (done) => {
+      chai.request(app)
+        .post('/api/v1/menu')
+        .set('x-access-token', adminToken)
+        .send({
+          foodName: 'foodName',
+          price: 1200,
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body.error).to.eql('description field is required');
+          done();
+        });
+    });
+
+    it('should return code 400 with error message when user tries to create menu with incorrect data type for description field', (done) => {
+      chai.request(app)
+        .post('/api/v1/menu')
+        .set('x-access-token', adminToken)
+        .send({
+          foodName: 'foodName',
+          price: 1200,
+          description: 1200
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body.error).to.eql('invalid type for description, must be a string');
+          done();
+        });
+    });
+
+    it('should return code 400 with error message when user tries to create menu with lengthy content in description field', (done) => {
+      chai.request(app)
+        .post('/api/v1/menu')
+        .set('x-access-token', adminToken)
+        .send({
+          foodName: 'foodName',
+          price: 1200,
+          description: 'okay, do not try to comprehend wat I am typing because I just want the length of the field to be greater than the max-limit htsaytsytyt sytasyu astsa sausaf saujhdsd dsiydsg duyg idusdf dsuydsu dsuydsgudsgyds ihyg dsjgd sdutds dsyd dsuyug dsdgu dsydsuy dsygdsu dsfdsyt sdufdsu sayfsau sayfsau sufds udsgfu sdutfs asuyfas usafu sauys dsuds udsgu sdugsdu usasf sftdgydfesrfeygd ydygysautsa sautsyts usfududyud uysd sygds dsydsigds dsiygsd dsuydsuydiu kugu i eeueoeouiiuewe oeoer eooiod dodod  ddodh doio reoireoer reoeroereroreoi rroruirehreuiureiureh reor reoreuorhreiuruitr trouitruiorthiutr truiotruoitrh toitoitroitroithtro troitroitroitroitr oi'
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body.error).to.eql('description too long, should not be more than 250 characters');
           done();
         });
     });
